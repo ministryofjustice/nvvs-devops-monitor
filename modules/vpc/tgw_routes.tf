a72cbfa3793086fc88e213e7f83a2a2915800955
Touhidul Islam <touhidul.islam@justice.gov.uk>
2022-10-10 11:26:37 +0100

    :recycle: rename attributes to be more specific about the gateway type
    so that terraform stops reporting a diff in the deployment stage

--- modules/vpc/tgw_routes.tf
+++ modules/vpc/tgw_routes.tf
@@ -2,7 +2,7 @@
 resource "aws_route" "corsham_mgmt" {
   count                  = var.enable_transit_gateway ? 1 : 0
   route_table_id         = module.vpc.public_route_table_ids[0]
-  gateway_id             = var.transit_gateway_id
+  transit_gateway_id     = var.transit_gateway_id
   destination_cidr_block = var.corsham_mgmt_range
 }
 
@@ -10,7 +10,7 @@ resource "aws_route" "corsham_mgmt" {
 resource "aws_route" "farnborough_mgmt" {
   count                  = var.enable_transit_gateway ? 1 : 0
   route_table_id         = module.vpc.public_route_table_ids[0]
-  gateway_id             = var.transit_gateway_id
+  transit_gateway_id     = var.transit_gateway_id
   destination_cidr_block = var.farnborough_mgmt_range
 }
 
@@ -18,7 +18,7 @@ resource "aws_route" "farnborough_mgmt" {
 resource "aws_route" "network_services" {
   count                  = var.enable_transit_gateway ? 1 : 0
   route_table_id         = module.vpc.public_route_table_ids[0]
-  gateway_id             = var.transit_gateway_id
+  transit_gateway_id     = var.transit_gateway_id
   destination_cidr_block = var.network_services_cidr_block
 }
 
@@ -26,14 +26,14 @@ resource "aws_route" "network_services" {
 # resource "aws_route" "mojo_dns_1" {
 #   count                  = var.enable_transit_gateway ? 1 : 0
 #   route_table_id         = module.vpc.default_route_table_id
-#   gateway_id             = var.transit_gateway_id
+#   transit_gateway_id     = var.transit_gateway_id
 #   destination_cidr_block = "${var.mojo_dns_ip_1}/32"
 # }
 
 # resource "aws_route" "mojo_dns_2" {
 #   count                  = var.enable_transit_gateway ? 1 : 0
 #   route_table_id         = module.vpc.default_route_table_id
-#   gateway_id             = var.transit_gateway_id
+#   transit_gateway_id     = var.transit_gateway_id
 #   destination_cidr_block = "${var.mojo_dns_ip_2}/32"
 # }
 
@@ -41,42 +41,42 @@ resource "aws_route" "network_services" {
 # resource "aws_route" "dom1_dns_1" {
 #   count                  = var.enable_transit_gateway ? 1 : 0
 #   route_table_id         = module.vpc.default_route_table_id
-#   gateway_id             = var.transit_gateway_id
+#   transit_gateway_id     = var.transit_gateway_id
 #   destination_cidr_block = "${var.dom1_dns_range_1}/24"
 # }
 
 # resource "aws_route" "dom1_dns_2" {
 #   count                  = var.enable_transit_gateway ? 1 : 0
 #   route_table_id         = module.vpc.default_route_table_id
-#   gateway_id             = var.transit_gateway_id
+#   transit_gateway_id     = var.transit_gateway_id
 #   destination_cidr_block = "${var.dom1_dns_range_2}/24"
 # }
 
 # resource "aws_route" "quantum_dns_1" {
 #   count                  = var.enable_transit_gateway ? 1 : 0
 #   route_table_id         = module.vpc.default_route_table_id
-#   gateway_id             = var.transit_gateway_id
+#   transit_gateway_id     = var.transit_gateway_id
 #   destination_cidr_block = "${var.quantum_dns_range_1}/24"
 # }
 
 # resource "aws_route" "quantum_dns_2" {
 #   count                  = var.enable_transit_gateway ? 1 : 0
 #   route_table_id         = module.vpc.default_route_table_id
-#   gateway_id             = var.transit_gateway_id
+#   transit_gateway_id     = var.transit_gateway_id
 #   destination_cidr_block = "${var.quantum_dns_range_2}/24"
 # }
 
 # resource "aws_route" "quantum_dns_3" {
 #   count                  = var.enable_transit_gateway ? 1 : 0
 #   route_table_id         = module.vpc.default_route_table_id
-#   gateway_id             = var.transit_gateway_id
+#   transit_gateway_id     = var.transit_gateway_id
 #   destination_cidr_block = "${var.quantum_dns_ip_1}/32"
 # }
 
 # resource "aws_route" "quantum_dns_4" {
 #   count                  = var.enable_transit_gateway ? 1 : 0
 #   route_table_id         = module.vpc.default_route_table_id
-#   gateway_id             = var.transit_gateway_id
+#   transit_gateway_id     = var.transit_gateway_id
 #   destination_cidr_block = "${var.quantum_dns_ip_2}/32"
 # }
 
@@ -84,7 +84,7 @@ resource "aws_route" "network_services" {
 # resource "aws_route" "psn_route_1" {
 #   count                  = var.enable_transit_gateway ? 1 : 0
 #   route_table_id         = module.vpc.default_route_table_id
-#   gateway_id             = var.transit_gateway_id
+#   transit_gateway_id     = var.transit_gateway_id
 #   destination_cidr_block = var.psn_team_protected_range_1
 # }
 
@@ -92,7 +92,7 @@ resource "aws_route" "network_services" {
 # resource "aws_route" "psn_route_2" {
 #   count                  = var.enable_transit_gateway ? 1 : 0
 #   route_table_id         = module.vpc.default_route_table_id
-#   gateway_id             = var.transit_gateway_id
+#   transit_gateway_id     = var.transit_gateway_id
 #   destination_cidr_block = var.psn_team_protected_range_2
 # }
 
@@ -100,7 +100,7 @@ resource "aws_route" "network_services" {
 # resource "aws_route" "sop_oci" {
 #   count                  = var.enable_transit_gateway ? 1 : 0
 #   route_table_id         = module.vpc.default_route_table_id
-#   gateway_id             = var.transit_gateway_id
+#   transit_gateway_id     = var.transit_gateway_id
 #   destination_cidr_block = var.sop_oci_range
 # }
 
@@ -108,7 +108,7 @@ resource "aws_route" "network_services" {
 # resource "aws_route" "corsham_5260" {
 #   count                  = var.enable_transit_gateway ? 1 : 0
 #   route_table_id         = module.vpc.default_route_table_id
-#   gateway_id             = var.transit_gateway_id
+#   transit_gateway_id     = var.transit_gateway_id
 #   destination_cidr_block = var.corsham_5260_ip
 # }
 
@@ -116,6 +116,6 @@ resource "aws_route" "network_services" {
 # resource "aws_route" "farnborough_5260" {
 #   count                  = var.enable_transit_gateway ? 1 : 0
 #   route_table_id         = module.vpc.default_route_table_id
-#   gateway_id             = var.transit_gateway_id
+#   transit_gateway_id     = var.transit_gateway_id
 #   destination_cidr_block = var.farnborough_5260_ip
 # }
