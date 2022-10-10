16ec2710a46394edfafa0d3fa55ccc52ed011248
Touhidul Islam <touhidul.islam@justice.gov.uk>
2022-10-10 11:09:38 +0100

    :recycle: remove lifecycle to no longer ignore desired size changes
    because of this block terraform was not changing the desired size

--- modules/eks/node_groups.tf
+++ modules/eks/node_groups.tf
@@ -14,10 +14,6 @@ resource "aws_eks_node_group" "green" {
     max_unavailable = 2
   }
 
-  lifecycle {
-    ignore_changes = [scaling_config[0].desired_size]
-  }
-
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
   depends_on = [
